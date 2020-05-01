b25ff80e286c6140e03e13efe07fa96d503aac67
hjz-z0 <2609120101@qq.com>
2020-05-01 22:40:13 +0800

    SpringBoot入门 idea上传github入门

--- src/main/java/com/example/demo/Demo2.java
+++ src/main/java/com/example/demo/Demo2.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("添加git文件测试！");
+        System.out.println("是大的撒打！");
     }
 }
